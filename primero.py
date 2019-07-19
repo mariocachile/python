194ffe5598922ef71123ebdcaf761ab232512565
mariocachile <mario.cachile@gmail.com>
2019-07-18 22:33:02 -0300

    nueva 2

--- primero.py
+++ primero.py
@@ -1,3 +1,4 @@
+
 a = 3 
 b= 2
 c= 25
@@ -12,9 +13,15 @@ for x in range(2,5):
 
 n=0
 sum=0
-max= 100000
+max= 10000000
+
 for n in range(1, max):
     # calculating sum
-    sum = sum + 1/n
+    sum = sum + 1/n**2
     
 print("suma de los ", max, "términos= ", sum)
+
+
+        
+        
+
