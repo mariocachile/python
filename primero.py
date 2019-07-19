b7f94dbf65d63e818a8c7e2433be208b87d05ee0
mariocachile <mario.cachile@gmail.com>
2019-07-18 22:07:22 -0300

    segundo cambio

--- primero.py
+++ primero.py
@@ -3,3 +3,22 @@ b= 2
 c= 25
 d=a+c+b
 
+for x in range(2,5):
+    print (x)
+
+
+
+sum=0
+for x in range(1, 6):
+    # calculating sum
+    sum = sum + x
+
+# displaying sum of first 5 natural numbers
+print("la suma es: ", sum)
+
+count = 0
+while (count < 3):     
+    count = count + 1
+    print("Hello Geek")
+
+print("unos")
