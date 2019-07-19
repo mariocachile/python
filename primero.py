1e2bc5367e3edaa1cf225526ca389ab55094d83c
mariocachile <mario.cachile@gmail.com>
2019-07-18 22:20:36 -0300

    corto

--- primero.py
+++ primero.py
@@ -8,20 +8,7 @@ for x in range(2,5):
 
 
 
-sum=0
-for x in range(1, 6):
-    # calculating sum
-    sum = sum + x
 
-# displaying sum of first 5 natural numbers
-print("la suma es: ", sum)
-
-count = 0
-while (count < 3):     
-    count = count + 1
-    print("Hello Geek")
-
-print("unos")
 
 n=0
 sum=0
